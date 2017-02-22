8d94b6c921f66490be4662a564e19eef3583abf3
Vladimir Kamarzin <vvk@vvk.pp.ru>
2017-02-22 14:42:23 +0500

    Add kitchen test for 'databag' source with 'simple' layout

--- recipes/test.rb
+++ recipes/test.rb
@@ -1,4 +1,13 @@
 # this recipe is intended to be used from test-kitchen
 
 user "test"
-ssh_private_key "test"
+ssh_private_key "test" do
+    source 'chef-vault'
+    layout 'simple'
+end
+
+user "test2"
+ssh_private_key "test2" do
+    source 'databag'
+    layout 'simple'
+end
